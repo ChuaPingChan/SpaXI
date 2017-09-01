c0947e57e15d8004d9ac6b41788cff421ebdc039
“marcusngwj” <marcusngwj@msn.com>
2017-09-02 00:43:33 +0800

    Add validation check for entity and synonym in QueryValidator

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/QueryValidator.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/QueryValidator.cpp
@@ -1,6 +1,6 @@
 #include "QueryValidator.h"
 
-
+using namespace std;
 
 QueryValidator::QueryValidator()
 {
@@ -9,4 +9,58 @@ QueryValidator::QueryValidator()
 
 QueryValidator::~QueryValidator()
 {
+}
+
+//Public methods
+bool QueryValidator::isValidQuery()
+{
+    return false;
+}
+
+//For Testing Private Methods
+bool QueryValidator::isValidEntityTest(string input)
+{
+    return isValidEnitity(input);
+}
+
+bool QueryValidator::isValidSynonymTest(string input)
+{
+    return isValidSynonym(input);
+}
+
+
+//Private methods
+
+vector<string> QueryValidator::getQueryToBeValidated()
+{
+    return vector<string>();
+}
+
+//Validation of Declaration
+bool QueryValidator::isValidDeclaration()
+{
+    return false;
+}
+
+bool QueryValidator::isValidEnitity(string input)
+{
+    string entityRegexString = "(procedure|stmtLst|stmt|assign|call|while|if|variable|constant|prog_line)";
+    regex entityRegex(entityRegexString);
+
+    return regex_match(input, entityRegex);
+}
+
+bool QueryValidator::isValidSynonym(string input)
+{
+    string synonymRegexString = "([[:alpha:]]{1})([[:alpha:]|[:digit:]|#]*)";
+    regex synonymRegex(synonymRegexString);
+
+    return regex_match(input, synonymRegex);
+}
+
+
+//Validation of Select
+bool QueryValidator::isValidSelect()
+{
+    return false;
 }
--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/QueryValidator.h
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/QueryValidator.h
@@ -1,8 +1,36 @@
 #pragma once
+#include <string>
+#include <vector>
+#include <list>
+#include <regex>
+
+using namespace std;
+
 class QueryValidator
 {
 public:
-	QueryValidator();
-	~QueryValidator();
+    QueryValidator();
+    ~QueryValidator();
+
+    bool isValidQuery();
+
+    //For Testing Private Methods
+    bool isValidEntityTest(string input);
+    bool isValidSynonymTest(string input);
+
+private: 
+    list<string> _synonymNameBank;
+
+    vector<string> getQueryToBeValidated();
+
+    //Validation of Declaration
+    bool isValidDeclaration();
+    
+    bool isValidEnitity(string input);
+    bool isValidSynonym(string input);
+
+
+    //Validation of Select
+    bool isValidSelect();
 };
 
