7d3885305189b35e03b6c29e78707c3b231dd904
wenhaohaoo <lwen.hao93@gmail.com>
2017-09-09 14:26:32 +0800

    initial implementation of QueryTree

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/QueryTree.h
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/QueryTree.h
@@ -1,8 +1,66 @@
 #pragma once
+#include <vector>
+#include <string>
+
+using namespace std;
+
 class QueryTree
 {
 public:
 	QueryTree();
 	~QueryTree();
+
+	void init();
+	void storeUnvalidatedStmts(vector<string> splitted);
+	void insertVariable(string type, string var);
+	void insertSuchThat(string clause);
+	void insertSelect(string stmt);
+	void insertPattern(string pattern);
+	
+	vector<string> getUnvalidatedStmts();
+	vector<string> getStmts();
+	vector<string> getAssigns();
+	vector<string> getWhiles();
+	vector<string> getVars();
+	vector<string> getConsts();
+	vector<string> getProgLines();
+	
+	int getNumVar();
+	int getNumSuchThat();
+
+	bool isInit();
+	bool isSplitted();
+	bool isValidated();
+	bool isEvaluated();
+
+	void setValidatedTrue();
+	void setEvaluatedTrue();
+
+	bool varExists(string var);
+
+private:
+	vector<string> unvalidatedStmts;
+	vector<string> stmts;
+	vector<string> assigns;
+	vector<string> whiles;
+	vector<string> vars;
+	vector<string> consts;
+	vector<string> progLines; 
+
+	string selectStmt;
+	string patternStmt;
+	vector<string> suchThatClauses;
+
+	int numVar = 0;
+
+	bool inited;
+	bool splitted;
+	bool validated;
+	bool evaluated;
+
+	void setInited(bool b);
+	void setSplitted(bool b);
+	void setValidated(bool b);
+	void setEvaluated(bool b);
 };
 
