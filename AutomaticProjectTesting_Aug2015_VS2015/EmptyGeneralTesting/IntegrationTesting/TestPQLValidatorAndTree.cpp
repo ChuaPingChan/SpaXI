d5f88da4ef00d8863b348238920b0e61c9f834dc
DESKTOP-H09F8Q1\Akankshita Dash <akankshitadash@yahoo.com>
2017-09-13 00:34:57 +0800

    Skeleton of Integration Testing

---
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/TestPQLValidatorAndTree.cpp
@@ -0,0 +1,21 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../SPA/PQL/QueryTree.h"
+#include "../SPA/PQL/QueryValidator.h"
+
+using namespace std;
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace IntegrationTesting
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            // TODO: Your test code here
+        }
+
+    };
+}
