bb83a60df9f82f9555a63e36b0b025d33bfd3465
wenhaohaoo <lwen.hao93@gmail.com>
2017-09-14 21:35:24 +0800

    added some some sub methods for evaluator

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/QueryEvaluator.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/QueryEvaluator.cpp
@@ -14,31 +14,190 @@ QueryEvaluator::~QueryEvaluator()
 void QueryEvaluator::evaluateFollows(array<string, 4> arr)
 {
 
-	if (arr[0] == "stmt")
+	if (arr[0] == "int" && arr[3] == "int")
 	{
 
 	}
+	else if (arr[0] == "int" && arr[3] == "_")
+	{
 
+	}
+	else if (arr[0] == "int" && arr[3] == "")
+	{
+
+	}
+	else if (arr[0] == "_" && arr[3] == "int")
+	{
+
+	}
+	else if (arr[0] == "_" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "_" && arr[3] == "")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "int")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "")
+	{
+
+	}
 }
 
 void QueryEvaluator::evaluateFollowsT(array<string, 4> arr)
 {
+	if (arr[0] == "int" && arr[3] == "int")
+	{
 
+	}
+	else if (arr[0] == "int" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "int" && arr[3] == "")
+	{
+
+	}
+	else if (arr[0] == "_" && arr[3] == "int")
+	{
+
+	}
+	else if (arr[0] == "_" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "_" && arr[3] == "")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "int")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "")
+	{
+
+	}
 }
 
 void QueryEvaluator::evaluateParent(array<string, 4> arr)
 {
+	if (arr[0] == "int" && arr[3] == "int")
+	{
 
+	}
+	else if (arr[0] == "int" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "int" && arr[3] == "")
+	{
+
+	}
+	else if (arr[0] == "_" && arr[3] == "int")
+	{
+
+	}
+	else if (arr[0] == "_" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "_" && arr[3] == "")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "int")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "")
+	{
+
+	}
 }
 
 void QueryEvaluator::evaluateParentT(array<string, 4> arr)
 {
+	if (arr[0] == "int" && arr[3] == "int")
+	{
 
+	}
+	else if (arr[0] == "int" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "int" && arr[3] == "")
+	{
+
+	}
+	else if (arr[0] == "_" && arr[3] == "int")
+	{
+
+	}
+	else if (arr[0] == "_" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "_" && arr[3] == "")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "int")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "")
+	{
+
+	}
 }
 
 void QueryEvaluator::evaluateUses(array<string, 4> arr)
 {
+	if (arr[0] == "int" && arr[3] == "int")
+	{
 
+	}
+	else if (arr[0] == "int" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "int" && arr[3] == "")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "int")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "_")
+	{
+
+	}
+	else if (arr[0] == "" && arr[3] == "")
+	{
+
+	}
 }
 
 void QueryEvaluator::evaluateModifies(array<string, 4> arr)
