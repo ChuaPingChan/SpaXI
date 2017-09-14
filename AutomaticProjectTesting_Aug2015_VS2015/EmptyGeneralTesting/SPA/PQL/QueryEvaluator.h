f9f3ec6404ab03358a38e1e6b0f308515bde4bae
wenhaohaoo <lwen.hao93@gmail.com>
2017-09-14 18:15:50 +0800

    initial implementation of evaluator

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/QueryEvaluator.h
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/QueryEvaluator.h
@@ -1,8 +1,22 @@
 #pragma once
+#include <string>
+#include <array>
+
+using namespace std;
+
 class QueryEvaluator
 {
 public:
 	QueryEvaluator();
 	~QueryEvaluator();
+
+private:
+	void evaluateFollows(array<string, 4> arr);
+	void evaluateFollowsT(array<string, 4> arr);
+	void evaluateParent(array<string, 4> arr);
+	void evaluateParentT(array<string, 4> arr);
+	void evaluateUses(array<string, 4> arr);
+	void evaluateModifies(array<string, 4> arr);
+	void evaluatePattern(array<string, 6> arr);
 };
 
