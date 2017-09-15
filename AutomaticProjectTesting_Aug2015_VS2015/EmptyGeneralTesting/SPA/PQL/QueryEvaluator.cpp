1cc9716a07ab1c41072db2527d5e7c90e1d686df
“marcusngwj” <marcusngwj@msn.com>
2017-09-15 19:21:49 +0800

    Structure of follow and parent

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/QueryEvaluator.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/QueryEvaluator.cpp
@@ -20,160 +20,160 @@ void QueryEvaluator::evaluateFollows(array<string, 4> arr)
 
 	if (arr[0] == "int" && arr[2] == "int")
 	{
-
+        //hasResult = isFollows(arr[1], arr[1]);
 	}
 	else if (arr[0] == "int" && arr[2] == "_")
 	{
-
+        //hasResult = isBeingFollowed(arr[1]);
 	}
 	else if (arr[0] == "int" && (arr[2] == "stmt" || arr[2] == "assign" || arr[2] == "while")) 
 	{
-
+        //getAfter(arr[0], arr[2]);
 	}
 	else if (arr[0] == "_" && arr[2] == "int")
 	{ 
-
+        //hasResult = isBeingFollowedBy(arr[3]);
 	}
 	else if (arr[0] == "_" && arr[2] == "_")
 	{
-
+        //hasResult = hasFollows();
 	}
 	else if (arr[0] == "_" && (arr[2] == "stmt" || arr[2] == "assign" || arr[2] == "while"))
 	{
-
+        //isFollowedBy(arr[2]);
 	}
 	else if ((arr[0] == "stmt" || arr[0] == "assign" || arr[0] == "while") && arr[2] == "int")
 	{
-
+        //isFollowedBy(arr[0], arr[2]);
 	}
 	else if ((arr[0] == "stmt" || arr[0] == "assign" || arr[0] == "while") && arr[2] == "_")
 	{
-
+        //getAfter(arr[0], arr[2]);
 	}
 	else if ((arr[0] == "stmt" || arr[0] == "assign" || arr[0] == "while") && (arr[2] == "stmt" || arr[2] == "assign" || arr[2] == "while"))
 	{
-
+        //getFollows(arr[0], arr[2];)
 	}
 }
 
 void QueryEvaluator::evaluateFollowsT(array<string, 4> arr)
 {
-	if (arr[0] == "int" && arr[2] == "int")
-	{
-
-	}
-	else if (arr[0] == "int" && arr[2] == "_")
-	{
-
-	}
-	else if (arr[0] == "int" && arr[2] == "")
-	{
-
-	}
-	else if (arr[0] == "_" && arr[2] == "int")
-	{
-
-	}
-	else if (arr[0] == "_" && arr[2] == "_")
-	{
-
-	}
-	else if (arr[0] == "_" && arr[2] == "")
-	{
-
-	}
-	else if (arr[0] == "" && arr[2] == "int")
-	{
-
-	}
-	else if (arr[0] == "" && arr[2] == "_")
-	{
-
-	}
-	else if (arr[0] == "" && arr[2] == "")
-	{
-
-	}
+    if (arr[0] == "int" && arr[2] == "int")
+    {
+        //hasResult = isFollowsT(arr[1], arr[1]);
+    }
+    else if (arr[0] == "int" && arr[2] == "_")
+    {
+        //hasResult = isBeingFollowedT(arr[1]);
+    }
+    else if (arr[0] == "int" && (arr[2] == "stmt" || arr[2] == "assign" || arr[2] == "while"))
+    {
+        //getAfterStar(arr[0], arr[2]);
+    }
+    else if (arr[0] == "_" && arr[2] == "int")
+    {
+        //hasResult = isBeingFollowedByStar(arr[3]);
+    }
+    else if (arr[0] == "_" && arr[2] == "_")
+    {
+        //hasResult = hasFollowsT();
+    }
+    else if (arr[0] == "_" && (arr[2] == "stmt" || arr[2] == "assign" || arr[2] == "while"))
+    {
+        //isFollowedStarBy(arr[2]);
+    }
+    else if ((arr[0] == "stmt" || arr[0] == "assign" || arr[0] == "while") && arr[2] == "int")
+    {
+        //isFollowedBy(arr[0], arr[2]);
+    }
+    else if ((arr[0] == "stmt" || arr[0] == "assign" || arr[0] == "while") && arr[2] == "_")
+    {
+        //getAfter(arr[0], arr[2]);
+    }
+    else if ((arr[0] == "stmt" || arr[0] == "assign" || arr[0] == "while") && (arr[2] == "stmt" || arr[2] == "assign" || arr[2] == "while"))
+    {
+        //getFollows(arr[0], arr[2];)
+    }
 }
 
 void QueryEvaluator::evaluateParent(array<string, 4> arr)
 {
 	if (arr[0] == "int" && arr[2] == "int")
 	{
-
+        //hasResult = isParent(arr[0], arr[2]);
 	}
 	else if (arr[0] == "int" && arr[2] == "_")
 	{
-
+        //hasResult = isParent(arr[2]);
 	}
-	else if (arr[0] == "int" && arr[2] == "")
+    else if (arr[0] == "int" && (arr[2] == "stmt" || arr[2] == "assign" || arr[2] == "while"))
 	{
-
+        //getChildren(arr[0], arr[2]);
 	}
 	else if (arr[0] == "_" && arr[2] == "int")
 	{
-
+        //hasResult = isChildren(arr[2]);
 	}
 	else if (arr[0] == "_" && arr[2] == "_")
 	{
-
+        //hasResult = hasParent();
 	}
-	else if (arr[0] == "_" && arr[2] == "")
+    else if (arr[0] == "_" && (arr[2] == "stmt" || arr[2] == "assign" || arr[2] == "while"))
 	{
-
+        //isParentOf(arr[2]);
 	}
-	else if (arr[0] == "" && arr[2] == "int")
+    else if ((arr[0] == "stmt" || arr[0] == "assign" || arr[0] == "while") && arr[2] == "int")
 	{
-
+        //isParentOf(arr[0], arr[2]);
 	}
-	else if (arr[0] == "" && arr[2] == "_")
+    else if ((arr[0] == "stmt" || arr[0] == "assign" || arr[0] == "while") && arr[2] == "_")
 	{
-
+        //getChildren(arr[0]);
 	}
-	else if (arr[0] == "" && arr[2] == "")
+    else if ((arr[0] == "stmt" || arr[0] == "assign" || arr[0] == "while") && (arr[2] == "stmt" || arr[2] == "assign" || arr[2] == "while"))
 	{
-
+        //getParent(arr[0], arr[2]);
 	}
 }
 
 void QueryEvaluator::evaluateParentT(array<string, 4> arr)
 {
-	if (arr[0] == "int" && arr[2] == "int")
-	{
+    if (arr[0] == "int" && arr[2] == "int")
+    {
 
-	}
-	else if (arr[0] == "int" && arr[2] == "_")
-	{
+    }
+    else if (arr[0] == "int" && arr[2] == "_")
+    {
 
-	}
-	else if (arr[0] == "int" && arr[2] == "")
-	{
+    }
+    else if (arr[0] == "int" && (arr[2] == "stmt" || arr[2] == "assign" || arr[2] == "while"))
+    {
 
-	}
-	else if (arr[0] == "_" && arr[2] == "int")
-	{
+    }
+    else if (arr[0] == "_" && arr[2] == "int")
+    {
 
-	}
-	else if (arr[0] == "_" && arr[2] == "_")
-	{
+    }
+    else if (arr[0] == "_" && arr[2] == "_")
+    {
 
-	}
-	else if (arr[0] == "_" && arr[2] == "")
-	{
+    }
+    else if (arr[0] == "_" && (arr[2] == "stmt" || arr[2] == "assign" || arr[2] == "while"))
+    {
 
-	}
-	else if (arr[0] == "" && arr[2] == "int")
-	{
+    }
+    else if ((arr[0] == "stmt" || arr[0] == "assign" || arr[0] == "while") && arr[2] == "int")
+    {
 
-	}
-	else if (arr[0] == "" && arr[2] == "_")
-	{
+    }
+    else if ((arr[0] == "stmt" || arr[0] == "assign" || arr[0] == "while") && arr[2] == "_")
+    {
 
-	}
-	else if (arr[0] == "" && arr[2] == "")
-	{
+    }
+    else if ((arr[0] == "stmt" || arr[0] == "assign" || arr[0] == "while") && (arr[2] == "stmt" || arr[2] == "assign" || arr[2] == "while"))
+    {
 
-	}
+    }
 }
 
 void QueryEvaluator::evaluateUses(array<string, 4> arr)
