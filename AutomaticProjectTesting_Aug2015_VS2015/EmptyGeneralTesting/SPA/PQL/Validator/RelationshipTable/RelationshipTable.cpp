396f5c05cd75c7d196268dbe97393d1497e9a304
DESKTOP-H09F8Q1\Akankshita Dash <akankshitadash@yahoo.com>
2017-09-28 22:27:19 +0800

    Table for Modifies and Uses

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Validator/RelationshipTable/RelationshipTable.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Validator/RelationshipTable/RelationshipTable.cpp
@@ -1,12 +1,47 @@
 #include "RelationshipTable.h"
 
 
+//Rule 1: SYNONYM (stmt|assign|while|if|prog_line|call|procedure)|STRING|INTEGER
+//Rule 2: SYNONYM (variable)|STRING|_
+//Rule 3: and so on....
+
+ ARGUMENT rule1 = { "stmt","assign","while","if","prog_line","call","procedure","STRING","INTEGER" };
+ ARGUMENT rule2 = { "variable","STRING","_" };
 
 RelationshipTable::RelationshipTable()
 {
+    ARGUMENT arg1, arg2, arg3;
+    
+    //Modifies & Uses
+    arg1 = rule1;
+    arg2 = rule2;
+    Relationship rModify(arg1, arg2);
+    Relationship rUses(arg1, arg2);
+    relTable[MODIFIES] = rModify;
+    relTable[USES] = rUses;
+    
 }
 
 
 RelationshipTable::~RelationshipTable()
 {
+}
+
+bool RelationshipTable::hasRelationship(RELATION inputRel)
+{
+   auto iterator = relTable.find(inputRel);
+   if(iterator==relTable.end())
+     return false;
+   
+   return true;
+}
+
+bool RelationshipTable::isArgValid(RELATION, ARGTYPE one, ARGTYPE two)
+{
+    return false;
+}
+
+bool RelationshipTable::isArgValid(RELATION, ARGTYPE one, ARGTYPE two, ARGTYPE three)
+{
+    return false;
 }
--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Validator/RelationshipTable/RelationshipTable.h
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Validator/RelationshipTable/RelationshipTable.h
@@ -1,14 +1,21 @@
 #pragma once
 #include"Relationship.h"
+#include<unordered_map>
 
 typedef string RELATION;
+typedef string ARGTYPE;
 
 class RelationshipTable
 {
 public:
     RelationshipTable();
     ~RelationshipTable();
-private:
 
+    bool hasRelationship(RELATION);
+    bool isArgValid(RELATION, ARGTYPE one, ARGTYPE two);
+    bool isArgValid(RELATION, ARGTYPE one, ARGTYPE two, ARGTYPE three);
+
+private:
+    unordered_map<RELATION, Relationship> relTable;
 };
 
