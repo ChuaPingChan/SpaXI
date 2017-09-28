d4eff77b872b46f0b812814dd964f8649d9c0ae0
ChuaPingChan <chua_pingchan@u.nus.edu>
2017-09-29 04:29:08 +0800

    Write up skeleton for ClauseResult.

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Utilities/ClauseResult.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Utilities/ClauseResult.cpp
@@ -1,9 +1,50 @@
 #include "ClauseResult.h"
 
+using namespace std;
+
 ClauseResult::ClauseResult()
 {
 }
 
 ClauseResult::~ClauseResult()
 {
+}
+
+vector<string> ClauseResult::getAllSynonyms()
+{
+    return vector<string>();
+}
+
+void ClauseResult::getSynonymResults(string synName, vector<string>* results)
+{
+}
+
+bool ClauseResult::synonymPresent(string synName)
+{
+    return false;
+}
+
+bool ClauseResult::addNewSynResults(string newSynName, vector<string> newSynResults)
+{
+    return false;
+}
+
+bool ClauseResult::removeCombinations(string synName, int value)
+{
+    return false;
+}
+
+bool ClauseResult::removeCombinations(string synName, string value)
+{
+    return false;
+}
+
+bool ClauseResult::pairWithOldSyn(string oldSyn, string oldSynValue, string newSyn, vector<string> newSynResults)
+{
+    return false;
+}
+
+bool ClauseResult::pairWithOldSyn(string oldSyn, int oldSynValue, string newSyn, vector<int> newSynResults)
+{
+    return false;
 }
--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Utilities/ClauseResult.h
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Utilities/ClauseResult.h
@@ -1,8 +1,35 @@
 #pragma once
+
+#include <unordered_map>
+
+using namespace std;
+
+/*
+This class is responsible for merging and storing the results of
+multiple PQL query clauses.
+*/
 class ClauseResult
 {
 public:
     ClauseResult();
     ~ClauseResult();
+
+    vector<string> getAllSynonyms();
+    void getSynonymResults(string synName, vector<string> *results);
+    void getSynonymResults(string synName, vector<int> *results);
+    bool synonymPresent(string synName);
+    bool addNewSynResults(string newSynName, vector<string> newSynResults);
+    bool addNewSynResults(string newSynName, vector<int> newSynResults);
+    bool removeCombinations(string synName, int value);
+    bool removeCombinations(string synName, string value);
+    bool pairWithOldSyn(string oldSyn, string oldSynValue, string newSyn, vector<string> newSynResults);
+    bool pairWithOldSyn(string oldSyn, int oldSynValue, string newSyn, vector<int> newSynResults);
+
+
+private:
+    unordered_map<string, int> _synToIdxMap;
+    vector<string> _synList;
+    vector<vector<int>> _intResult;
+    vector<vector<string>> _stringResult;
 };
 
