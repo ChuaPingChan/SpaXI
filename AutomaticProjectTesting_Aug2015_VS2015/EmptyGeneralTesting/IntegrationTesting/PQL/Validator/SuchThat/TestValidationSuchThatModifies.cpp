51d53277355e6d7f23f18de0e97adc4bfa068db0
“marcusngwj” <marcusngwj@msn.com>
2017-10-01 03:02:49 +0800

    Add new test

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/PQL/Validator/SuchThat/TestValidationSuchThatModifies.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/PQL/Validator/SuchThat/TestValidationSuchThatModifies.cpp
@@ -2,6 +2,8 @@
 #include "string.h"
 #include "..\SPA\PQL\QueryTree.h"
 #include "..\SPA\PQL\Validator\Selection\SuchThat\SuchThatHandler.h"
+#include "..\SPA\PQL\Utilities\SuchThatClause.h"
+#include "..\..\Utility\UtilitySuchThat.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
@@ -11,15 +13,81 @@ namespace UnitTesting
     TEST_CLASS(TestValidationSuchThatModifies)
     {
     public:
-
         TEST_METHOD(TestValidity_SuchThatModifies_Int_IdentWithQuotes_Valid)
         {
             string str = "Modifies(1, \"x\")";
-            QueryTree qtPtr;
-            SuchThatHandler stHandler = SuchThatHandler(&qtPtr);
-            bool isValid = stHandler.isValidSuchThat(str);
-            Assert::IsTrue(isValid);
-            //Assert::IsTrue(UtilityQueryTree::isGetAllAssignsSame(qtPtr, fdv.getQueryTreeCopy()));
+            QueryTree qt;
+            SuchThatHandler stHandler = SuchThatHandler(&qt);
+            Assert::IsTrue(stHandler.isValidSuchThat(str));
+            SuchThatClause expected = UtilitySuchThat::makeSuchThatClause(0, 8, "1", 10, "\"x\"");
+            SuchThatClause actual = UtilitySuchThat::getFirstClauseFromTree(qt);
+            Assert::IsTrue(UtilitySuchThat::isSameSuchThatClauseContent(expected, actual));
         }
+
+        TEST_METHOD(TestValidity_SuchThatModifies_Int_Underscore_Valid)
+        {
+            string str = "Modifies(1, _)";
+            QueryTree qt;
+            SuchThatHandler stHandler = SuchThatHandler(&qt);
+            Assert::IsTrue(stHandler.isValidSuchThat(str));
+            SuchThatClause expected = UtilitySuchThat::makeSuchThatClause(0, 8, "1", 9, "_");
+            SuchThatClause actual = UtilitySuchThat::getFirstClauseFromTree(qt);
+            Assert::IsTrue(UtilitySuchThat::isSameSuchThatClauseContent(expected, actual));
+        }
+
+        TEST_METHOD(TestValidity_SuchThatModifies_Int_Variable_Valid)
+        {
+            string str = "Modifies(1, v)";
+            QueryTree qt;
+            qt.insertVariable("variable", "v");
+            SuchThatHandler stHandler = SuchThatHandler(&qt);
+            Assert::IsTrue(stHandler.isValidSuchThat(str));
+            SuchThatClause expected = UtilitySuchThat::makeSuchThatClause(0, 8, "1", 7, "v");
+            SuchThatClause actual = UtilitySuchThat::getFirstClauseFromTree(qt);
+            Assert::IsTrue(UtilitySuchThat::isSameSuchThatClauseContent(expected, actual));
+        }
+
+        TEST_METHOD(TestValidity_SuchThatModifies_Synonym_IdentWithQuotes_Valid)
+        {
+            string str = "Modifies(a, \"x\")";
+            QueryTree qt;
+            qt.insertVariable("assign", "a");
+            SuchThatHandler stHandler = SuchThatHandler(&qt);
+            Assert::IsTrue(stHandler.isValidSuchThat(str));
+            SuchThatClause expected = UtilitySuchThat::makeSuchThatClause(0, 1, "a", 10, "\"x\"");
+            SuchThatClause actual = UtilitySuchThat::getFirstClauseFromTree(qt);
+            Assert::IsTrue(UtilitySuchThat::isSameSuchThatClauseContent(expected, actual));
+        }
+
+        TEST_METHOD(TestValidity_SuchThatModifies_Synonym_Underscore_Valid)
+        {
+            string str = "Modifies(s, _)";
+            QueryTree qt;
+            qt.insertVariable("stmt", "s");
+            SuchThatHandler stHandler = SuchThatHandler(&qt);
+            Assert::IsTrue(stHandler.isValidSuchThat(str));
+            SuchThatClause expected = UtilitySuchThat::makeSuchThatClause(0, 0, "s", 9, "_");
+            SuchThatClause actual = UtilitySuchThat::getFirstClauseFromTree(qt);
+            Assert::IsTrue(UtilitySuchThat::isSameSuchThatClauseContent(expected, actual));
+        }
+
+        TEST_METHOD(TestValidity_SuchThatModifies_Synonym_Variable_Valid)
+        {
+            string str = "Modifies(w, v)";
+            QueryTree qt;
+            qt.insertVariable("while", "w");
+            qt.insertVariable("variable", "v");
+            SuchThatHandler stHandler = SuchThatHandler(&qt);
+            Assert::IsTrue(stHandler.isValidSuchThat(str));
+            SuchThatClause expected = UtilitySuchThat::makeSuchThatClause(0, 2, "w", 7, "v");
+            SuchThatClause actual = UtilitySuchThat::getFirstClauseFromTree(qt);
+            Assert::IsTrue(UtilitySuchThat::isSameSuchThatClauseContent(expected, actual));
+        }
+
+        /******************************************************************************************/
+        //TODO: Add test when procedure is inplemented [-- HERE --- ]
+        /******************************************************************************************/
+
+
     };
 }
