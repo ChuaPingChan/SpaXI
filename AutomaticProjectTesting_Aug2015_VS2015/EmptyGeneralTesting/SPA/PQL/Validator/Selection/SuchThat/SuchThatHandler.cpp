12ce799f7a7d4ff7faf9eabef4f8f701c26aba6f
“marcusngwj” <marcusngwj@msn.com>
2017-09-30 19:09:49 +0800

    Add static strings

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Validator/Selection/SuchThat/SuchThatHandler.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Validator/Selection/SuchThat/SuchThatHandler.cpp
@@ -10,7 +10,31 @@ SuchThatHandler::~SuchThatHandler()
 {
 }
 
+static const string MODIFIES = "Modifies";
+static const string USES = "Uses";
+static const string PARENT = "Parent";
+static const string PARENTSTAR = "Parent*";
+static const string FOLLOWS = "Follows";
+static const string FOLLOWSSTAR = "Follows*";
+static const string CALLS = "Calls";
+static const string CALLSSTAR = "Calls*";
+static const string NEXT = "Next";
+static const string NEXTSTAR = "Next*";
+static const string AFFECTS = "Affects";
+static const string AFFECTSSTAR = "Affects*";
 
+static const string STMT = "stmt";
+static const string ASSIGN = "assign";
+static const string WHILE = "while";
+static const string IF = "if";
+static const string PROG_LINE = "prog_line";
+static const string CALL = "call";
+static const string PROCEDURE = "procedure";
+static const string VARIABLE = "variable";
+static const string INTEGER = "int";
+static const string UNDERSCORE = "underscore";
+static const string IDENT_WITH_QUOTES = "identWithQuotes";
+static const string CONSTANT = "constant";
 
 bool SuchThatHandler::isValidSuchThat(string str)
 {
