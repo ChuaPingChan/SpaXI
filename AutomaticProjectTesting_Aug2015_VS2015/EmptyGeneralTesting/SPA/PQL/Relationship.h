6d86c4aaef620afa89f0b3e7ec8d9f2b711e9397
“marcusngwj” <marcusngwj@msn.com>
2017-10-01 18:59:20 +0800

    add relationship enum

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Relationship.h
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Relationship.h
@@ -1,1 +1,33 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
+enum RELATIONSHIP
+{
+    MODIFIES = 0,
+    USES,
+    PARENT,
+    PARENTSTAR,
+    FOLLOWS,
+    FOLLOWSSTAR,
+    CALLS,
+    CALLSSTAR,
+    NEXT,
+    NEXTSTAR,
+    AFFECTS,
+    AFFECTSSTAR
+};
+
+static const string RELATIONSHIP_STRING[] = { "Modifies",
+                                            "Uses",
+                                            "Parent",
+                                            "Parent*",
+                                            "Follows",
+                                            "Follows*",
+                                            "Calls",
+                                            "Calls*",
+                                            "Next",
+                                            "Next*",
+                                            "Affects",
+                                            "Affects*" }
