4c30e6ef4c7ab9a75dc15a60654899754abe2a75
DESKTOP-H09F8Q1\Akankshita Dash <akankshitadash@yahoo.com>
2017-10-08 14:54:05 +0800

    Skeleton for Calls

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Validator/Selection/SuchThat/CallsValidator.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Validator/Selection/SuchThat/CallsValidator.cpp
@@ -1,12 +1,28 @@
 #include "CallsValidator.h"
 
-
-
-CallsValidator::CallsValidator()
+CallsValidator::CallsValidator(Relationship rel, string paramStr, QueryTree * qtPtrNew)
 {
 }
 
-
 CallsValidator::~CallsValidator()
 {
+}
+
+void CallsValidator::validate()
+{
+}
+
+bool CallsValidator::isValid()
+{
+    return false;
+}
+
+bool CallsValidator::isValidArgOne(string argOne)
+{
+    return false;
+}
+
+bool CallsValidator::isValidArgTwo(string argTwo)
+{
+    return false;
 }
