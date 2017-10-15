c30810938b9fae7aa24077548d5df48c1e14b7a7
“marcusngwj” <marcusngwj@msn.com>
2017-10-15 22:48:48 +0800

    Add test 3 from old iteration 1 test

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/Release/AutoTester.bat
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/Release/AutoTester.bat
@@ -5,7 +5,7 @@ echo ==========================================================
 echo [0] Generate all queries from xls to txt
 echo [1] Run AutoTester for test 1
 echo [2] Run AutoTester for test 2
-::echo [3] Run AutoTester for test 3
+echo [3] Run AutoTester for test 3
 echo [a] Run AutoTester for all tests
 echo [x] Exit
 set option=
@@ -13,21 +13,27 @@ set /p option=Please select your options:
 if '%option%'=='0' (
 	goto :generateQueries
 )
-if '%option%'=='1' (
-	goto :runTest1
-)
-if '%option%'=='2' (
-	goto :runTest2
-)
 if '%option%'=='a' (
 	goto :runAllTests
 )
 if '%option%'=='x' (
 	goto :close
 )
+if '%option%'=='1' (
+	goto :runTest1
+)
+if '%option%'=='2' (
+	goto :runTest2
+)
+if '%option%'=='3' (
+	goto :runTest3
+)
 
 goto :main
 echo ==========================================================
+
+:close
+goto eof
 
 :generateQueries
 @echo on
@@ -47,6 +53,7 @@ python Test2/QueryGenerator.py
 ::Run all AutoTester
 AutoTester Test1/test1.txt Test1/testQuery1.txt Test1/outTest1.xml
 AutoTester Test2/ZYTest.txt Test2/testQuery2.txt Test2/outTest2.xml
+AutoTester Test3/test3source.txt Test3/test3query.txt Test3/outTest3.xml
 @echo off
 pause
 cls
@@ -72,5 +79,11 @@ pause
 cls
 goto :main
 
-:close
-goto eof
+:runTest3
+@echo on
+echo Running AutoTester ...
+AutoTester Test3/test3source.txt Test3/test3query.txt Test3/outTest3.xml
+@echo off
+pause
+cls
+goto :main
