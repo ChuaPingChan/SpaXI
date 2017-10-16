926419d0beefceac0ac953e72e821946be2655bb
Ying <ying1996@gmail.com>
2017-10-16 21:58:17 +0800

    remove duplicate next

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PKB/NextTable.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PKB/NextTable.cpp
@@ -6,11 +6,14 @@ NextTable::NextTable() {
 }
 
 bool NextTable::setNextRel(int stmt, int stmtNext) {
+	string nextRel = to_string(stmt) + "," + to_string(stmtNext);
+	if (allNextRel.find(nextRel) != allNextRel.end()) {
+		return true;
+	}
 	nextMap[stmt].push_back(stmtNext);
 	nextMapReverse[stmtNext].push_back(stmt);
 	prevList.push_back(stmt);
 	nextList.push_back(stmtNext);
-	string nextRel = to_string(stmt) + "," + to_string(stmtNext);
 	allNextRel.insert(nextRel);
 	return true;
 }
