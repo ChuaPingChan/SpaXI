307ad32dc3ec7db63ee1b7322094d2031b633072
“marcusngwj” <marcusngwj@msn.com>
2017-10-18 11:58:00 +0800

    Write test for pattern assign bug found - extract 2nd arg

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/PQL/Validator/Pattern/TestValidationPatternAssign.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/PQL/Validator/Pattern/TestValidationPatternAssign.cpp
@@ -70,6 +70,18 @@ namespace UnitTesting
             Assert::IsTrue(UtilitySelection::isSamePatternClauseAssignWhileContent(expected, actual));
         }
 
+        TEST_METHOD(TestValidity_Pattern_Assign_Underscore_ExpressionSpec_ExactMatch_SingleVariable_Valid)
+        {
+            string str = "a(_, \"x\")";
+            QueryTree qt;
+            qt.insertSynonym(ASSIGN, "a");
+            PatternHandler pHandler = PatternHandler(&qt);
+            Assert::IsTrue(pHandler.isValidPattern(str));
+            PatternClause expected = UtilitySelection::makePatternClause(ASSSIGN_PATTERN, "a", UNDERSCORE, "_", EXPRESSION_SPEC_EXACT, "x");
+            PatternClause actual = UtilitySelection::getFirstPatternClauseFromTree(qt);
+            Assert::IsTrue(UtilitySelection::isSamePatternClauseAssignWhileContent(expected, actual));
+        }
+
         TEST_METHOD(TestValidity_Pattern_Assign_IdentWithQuotes_ExpressionSpec_PartialMatch_SingleVariable_Valid)
         {
             string str = "a(\"x\", _\"x\"_)";
@@ -78,6 +90,18 @@ namespace UnitTesting
             PatternHandler pHandler = PatternHandler(&qt);
             Assert::IsTrue(pHandler.isValidPattern(str));
             PatternClause expected = UtilitySelection::makePatternClause(ASSSIGN_PATTERN, "a", IDENT_WITHQUOTES, "x", EXPRESSION_SPEC_PARTIAL, "x");
+            PatternClause actual = UtilitySelection::getFirstPatternClauseFromTree(qt);
+            Assert::IsTrue(UtilitySelection::isSamePatternClauseAssignWhileContent(expected, actual));
+        }
+
+        TEST_METHOD(TestValidity_Pattern_Assign_IdentWithQuotes_ExpressionSpec_ExactMatch_SingleVariable_Valid)
+        {
+            string str = "a(\"x\", \"x\")";
+            QueryTree qt;
+            qt.insertSynonym(ASSIGN, "a");
+            PatternHandler pHandler = PatternHandler(&qt);
+            Assert::IsTrue(pHandler.isValidPattern(str));
+            PatternClause expected = UtilitySelection::makePatternClause(ASSSIGN_PATTERN, "a", IDENT_WITHQUOTES, "x", EXPRESSION_SPEC_EXACT, "x");
             PatternClause actual = UtilitySelection::getFirstPatternClauseFromTree(qt);
             Assert::IsTrue(UtilitySelection::isSamePatternClauseAssignWhileContent(expected, actual));
         }
@@ -106,7 +130,7 @@ namespace UnitTesting
             Assert::IsTrue(UtilitySelection::isSamePatternClauseAssignWhileContent(expected, actual));
         }
 
-        TEST_METHOD(TestValidity_Pattern_Assign_Synonym_ExpressionSpec_PartialMatch_MultiVariable_Valid)
+        TEST_METHOD(TestValidity_Pattern_Assign_Synonym_ExpressionSpec_MultiVariable_SingleOperator_PartialMatch_Valid)
         {
             string str = "a(v, _\"x+y\"_)";
             QueryTree qt;
@@ -115,6 +139,71 @@ namespace UnitTesting
             PatternHandler pHandler = PatternHandler(&qt);
             Assert::IsTrue(pHandler.isValidPattern(str));
             PatternClause expected = UtilitySelection::makePatternClause(ASSSIGN_PATTERN, "a", VARIABLE, "v", EXPRESSION_SPEC_PARTIAL, "x+y");
+            PatternClause actual = UtilitySelection::getFirstPatternClauseFromTree(qt);
+            Assert::IsTrue(UtilitySelection::isSamePatternClauseAssignWhileContent(expected, actual));
+        }
+
+        TEST_METHOD(TestValidity_Pattern_Assign_Synonym_ExpressionSpec_MultiVariable_SingleOperator_ExactMatch_Valid)
+        {
+            string str = "a(v, \"x+y\")";
+            QueryTree qt;
+            qt.insertSynonym(ASSIGN, "a");
+            qt.insertSynonym(VARIABLE, "v");
+            PatternHandler pHandler = PatternHandler(&qt);
+            Assert::IsTrue(pHandler.isValidPattern(str));
+            PatternClause expected = UtilitySelection::makePatternClause(ASSSIGN_PATTERN, "a", VARIABLE, "v", EXPRESSION_SPEC_EXACT, "x+y");
+            PatternClause actual = UtilitySelection::getFirstPatternClauseFromTree(qt);
+            Assert::IsTrue(UtilitySelection::isSamePatternClauseAssignWhileContent(expected, actual));
+        }
+
+        TEST_METHOD(TestValidity_Pattern_Assign_Synonym_ExpressionSpec_MultiVariable_MultiOperator_ExactMatch_Valid)
+        {
+            string str = "a(v, \"x+y-z*a\")";
+            QueryTree qt;
+            qt.insertSynonym(ASSIGN, "a");
+            qt.insertSynonym(VARIABLE, "v");
+            PatternHandler pHandler = PatternHandler(&qt);
+            Assert::IsTrue(pHandler.isValidPattern(str));
+            PatternClause expected = UtilitySelection::makePatternClause(ASSSIGN_PATTERN, "a", VARIABLE, "v", EXPRESSION_SPEC_EXACT, "x+y-z*a");
+            PatternClause actual = UtilitySelection::getFirstPatternClauseFromTree(qt);
+            Assert::IsTrue(UtilitySelection::isSamePatternClauseAssignWhileContent(expected, actual));
+        }
+
+        TEST_METHOD(TestValidity_Pattern_Assign_Synonym_ExpressionSpec_MultiInteger_MultiOperator_ExactMatch_Valid)
+        {
+            string str = "a(v, \"1+3*4-9\")";
+            QueryTree qt;
+            qt.insertSynonym(ASSIGN, "a");
+            qt.insertSynonym(VARIABLE, "v");
+            PatternHandler pHandler = PatternHandler(&qt);
+            Assert::IsTrue(pHandler.isValidPattern(str));
+            PatternClause expected = UtilitySelection::makePatternClause(ASSSIGN_PATTERN, "a", VARIABLE, "v", EXPRESSION_SPEC_EXACT, "1+3*4-9");
+            PatternClause actual = UtilitySelection::getFirstPatternClauseFromTree(qt);
+            Assert::IsTrue(UtilitySelection::isSamePatternClauseAssignWhileContent(expected, actual));
+        }
+
+        TEST_METHOD(TestValidity_Pattern_Assign_Synonym_ExpressionSpec_ExtendedComplexExpression_NoBrackets_ExactMatch_Valid)
+        {
+            string str = "a(v, \"hell0+15-m3*im-w0ndering-a11+th353*y3ars-youd+lik3*2*m33t\")";
+            QueryTree qt;
+            qt.insertSynonym(ASSIGN, "a");
+            qt.insertSynonym(VARIABLE, "v");
+            PatternHandler pHandler = PatternHandler(&qt);
+            Assert::IsTrue(pHandler.isValidPattern(str));
+            PatternClause expected = UtilitySelection::makePatternClause(ASSSIGN_PATTERN, "a", VARIABLE, "v", EXPRESSION_SPEC_EXACT, "hell0+15-m3*im-w0ndering-a11+th353*y3ars-youd+lik3*2*m33t");
+            PatternClause actual = UtilitySelection::getFirstPatternClauseFromTree(qt);
+            Assert::IsTrue(UtilitySelection::isSamePatternClauseAssignWhileContent(expected, actual));
+        }
+
+        TEST_METHOD(TestValidity_Pattern_Assign_Synonym_ExpressionSpec_ExtendedComplexExpression_WithBrackets_ExactMatch_Valid)
+        {
+            string str = "a(v, \"(1-(wanna+(b3*(th3-very+best)-like*(no+one*ever-was)))*To-catch+them+is-((my*real+test)*ToTrainThem+isMyCause))\")";
+            QueryTree qt;
+            qt.insertSynonym(ASSIGN, "a");
+            qt.insertSynonym(VARIABLE, "v");
+            PatternHandler pHandler = PatternHandler(&qt);
+            Assert::IsTrue(pHandler.isValidPattern(str));
+            PatternClause expected = UtilitySelection::makePatternClause(ASSSIGN_PATTERN, "a", VARIABLE, "v", EXPRESSION_SPEC_EXACT, "(1-(wanna+(b3*(th3-very+best)-like*(no+one*ever-was)))*To-catch+them+is-((my*real+test)*ToTrainThem+isMyCause))");
             PatternClause actual = UtilitySelection::getFirstPatternClauseFromTree(qt);
             Assert::IsTrue(UtilitySelection::isSamePatternClauseAssignWhileContent(expected, actual));
         }
