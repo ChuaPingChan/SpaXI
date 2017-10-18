8970c1f9e5c46dd7a60e180b871c0a0cbcd03d26
“marcusngwj” <marcusngwj@msn.com>
2017-10-18 19:37:14 +0800

    Update bat file for easier testing

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/Release/AutoTester.bat
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/Release/AutoTester.bat
@@ -1,20 +1,16 @@
 @echo off
 :main
-echo Welcome to Team 11 AutoTester
-echo ==========================================================
-echo [0] Generate all queries from xls to txt
-echo [1] Run AutoTester for test 1
-echo [2] Run AutoTester for test 2
-echo [3] Run AutoTester for test 3
-echo [a] Run AutoTester for all tests
+echo ===============================================================================
+echo Welcome to Team 11 AutoTester (o.o)
+echo ===============================================================================
+echo Type the test index to run the test
+echo [0] Receive a compliment
+echo [?] Run AutoTester for test ?
 echo [x] Exit
 set option=
 set /p option=Please select your options: 
 if '%option%'=='0' (
-	goto :generateQueries
-)
-if '%option%'=='a' (
-	goto :runAllTests
+	goto :generateCompliment
 )
 if '%option%'=='x' (
 	goto :close
@@ -28,62 +24,68 @@ if '%option%'=='2' (
 if '%option%'=='3' (
 	goto :runTest3
 )
-
+if '%option%'=='4' (
+	goto :runTest4
+)
 goto :main
-echo ==========================================================
+echo ===============================================================================
 
 :close
 goto eof
 
-:generateQueries
-@echo on
-python Test1/QueryGenerator.py
-python Test2/QueryGenerator.py
+:generateCompliment
 @echo off
-pause
-cls
-goto :main
-
-:runAllTests
-@echo on
-echo Running AutoTester ...
-::Run all python files
-python Test1/QueryGenerator.py
-python Test2/QueryGenerator.py
-::Run all AutoTester
-AutoTester Test1/test1.txt Test1/testQuery1.txt Test1/outTest1.xml
-AutoTester Test2/ZYTest.txt Test2/testQuery2.txt Test2/outTest2.xml
-AutoTester Test3/test3source.txt Test3/test3query.txt Test3/outTest3.xml
-@echo off
+echo.
+echo You are beautiful :) Thank you for teaching us!
+echo.
 pause
 cls
 goto :main
 
 :runTest1
-@echo on
-python Test1/QueryGenerator.py
-echo Running AutoTester ...
-AutoTester Test1/test1.txt Test1/testQuery1.txt Test1/outTest1.xml
-@echo off
+echo ===============================================================================
+echo Generating queries for test 1...
+python Test1_Frozen\QueryGenerator1.py Test1_Frozen\QuerySet1.xlsx Test1_Frozen\test1query.txt
+echo Finish generating queries for test 1.
+echo Running AutoTester for test 1...
+AutoTester Test1_Frozen\test1source.txt Test1_Frozen\test1query.txt Test1_Frozen\out1.xml> Test1_Frozen\AutoTester1.txt
+echo Finish running AutoTester for test 1.
 pause
 cls
 goto :main
 
 :runTest2
-@echo on
-python Test2/QueryGenerator.py
-echo Running AutoTester ...
-AutoTester Test2/ZYTest.txt Test2/testQuery2.txt Test2/outTest2.xml
-@echo off
+echo ===============================================================================
+echo Generating Queries for test 2...
+python Test2\QueryGenerator2.py Test2\QuerySet2.xlsx Test2\test2query.txt
+echo Finish generating queries for test 2.
+echo Running AutoTester for test 2...
+AutoTester Test2\test2source.txt Test2\test2query.txt Test2\out2.xml> Test2\AutoTester2.txt
+echo Finish running AutoTester for test 2.
 pause
 cls
 goto :main
 
 :runTest3
-@echo on
-echo Running AutoTester ...
-AutoTester Test3/test3source.txt Test3/test3query.txt Test3/outTest3.xml
-@echo off
+echo ===============================================================================
+echo Running AutoTester for test 3...
+AutoTester Test3_Frozen\test3source.txt Test3_Frozen\test3query.txt Test3_Frozen\out3.xml > Test3_Frozen\AutoTester.txt
+echo Finish running AutoTester for test 3.
+pause
+cls
+goto :main
+
+:runTest4
+echo ===============================================================================
+echo Running AutoTester for test 4_0...
+AutoTester Test4_Frozen\test4source0.txt Test4_Frozen\test4query0.txt Test4_Frozen\out4_0.xml > Test4_Frozen\AutoTester4_0.txt
+echo Finish running AutoTester for test 4_0.
+echo Running AutoTester for test 4_1...
+AutoTester Test4_Frozen\test4source1.txt Test4_Frozen\test4query1.txt Test4_Frozen\out4_1.xml > Test4_Frozen\AutoTester4_1.txt
+echo Finish running AutoTester for test 4_1.
+echo Running AutoTester for test 4_2...
+AutoTester Test4_Frozen\test4source2.txt Test4_Frozen\test4query2.txt Test4_Frozen\out4_2.xml > Test4_Frozen\AutoTester4_2.txt
+echo Finish running AutoTester for test 4_2.
 pause
 cls
 goto :main
