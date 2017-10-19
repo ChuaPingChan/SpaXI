d1dd1c39d4537ec6f8fb66fde68112e17dbee3e5
DESKTOP-M5TPFC0\Akankshita Dash <akankshitadash@yahoo.com>
2017-10-19 20:47:00 +0800

    Unit tests for ResultFormatter Select BOOLEAN false

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/UnitTesting/TestResultFormatter.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/UnitTesting/TestResultFormatter.cpp
@@ -15,13 +15,50 @@ namespace UnitTesting
         * Return false result *
         ************************/
 
-       /* TEST_METHOD(TestRegex_InvalidDeclaration_False)
+        TEST_METHOD(TestRegex_EmptyDeclaration_FalseResult)
         {
             ResultFormatter rf;
-            string query = "stmt s;Select BOOLEAN";
+            string query = ";Select BOOLEAN";
             list<string> expectedResult = { "false" };
             Assert::IsTrue(rf.handleInvalidQuery(query) == expectedResult);
-        }*/
+        }
 
+        TEST_METHOD(TestRegex_InvalidDeclaration_FalseResult)
+        {
+            ResultFormatter rf;
+            string query = "stmt 12739813279; Select BOOLEAN";
+            list<string> expectedResult = { "false" };
+            Assert::IsTrue(rf.handleInvalidQuery(query) == expectedResult);
+        }
+
+        TEST_METHOD(TestRegex_InvalidQueryRelation_FalseResult)
+        {
+            ResultFormatter rf;
+            string query = "stmt s;Select BOOLEAN such that Bag(1, 2)";
+            list<string> expectedResult = { "false" };
+            Assert::IsTrue(rf.handleInvalidQuery(query) == expectedResult);
+        }
+
+        TEST_METHOD(TestRegex_InvalidDeclarationWithMultipleSemiColons_FalseResult)
+        {
+            ResultFormatter rf;
+            string query = "stmt s;;;;;;;;;;; Select BOOLEAN";
+            list<string> expectedResult = { "false" };
+            Assert::IsTrue(rf.handleInvalidQuery(query) == expectedResult);
+        }
+
+        TEST_METHOD(TestRegex_QueryWithInvalidStart_EmptyResult)
+        {
+            ResultFormatter rf;
+            string query = "stmt s;Random Select BOOLEAN";
+            Assert::IsTrue(rf.handleInvalidQuery(query).empty());
+        }
+
+        TEST_METHOD(TestRegex_ValidDeclarationFollowedByInvalidDeclaration_EmptyResult)
+        {
+            ResultFormatter rf;
+            string query = "stmt s; 3791237937897192# Select BOOLEAN";
+            Assert::IsTrue(rf.handleInvalidQuery(query).empty());
+        }
     };
 }
