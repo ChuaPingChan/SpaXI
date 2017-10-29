1628c32ab4d3c34c361937b9d68464e8c5ebf887
“marcusngwj” <marcusngwj@msn.com>
2017-10-29 12:17:51 +0800

    Update bat file and resources in test 10

---
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/Release/Test10_Loop/GenerateQuery.bat
@@ -0,0 +1,5 @@
+python GenerateQuery.py ExcelQuery.xlsx QueryAffects.txt Affects
+python GenerateQuery.py ExcelQuery.xlsx QueryAffectsStar.txt AffectsStar
+python GenerateQuery.py ExcelQuery.xlsx QueryModifies.txt Modifies
+python GenerateQuery.py ExcelQuery.xlsx QueryNext.txt Next
+pause
