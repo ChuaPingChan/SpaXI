ebd43a6ec2d241f35045507bc553c615e89bb0f4
ChuaPingChan <chua_pingchan@u.nus.edu>
2017-10-30 23:45:00 +0800

    Add unit test skeleton for ClauseGroupManager.

---
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/UnitTesting/PQL/Optimizer/TestClauseGroupManager.cpp
@@ -0,0 +1,55 @@
+#include "CppUnitTest.h"
+#include <string>
+#include <queue>
+
+#include "../SPA/PQL/Optimizer/Optimizer.h"
+#include "../SPA/PQL/QueryTree.h"
+#include "../SPA/PQL/Utilities/Clause.h"
+#include "../SPA/PQL/Utilities/SuchThatClause.h"
+#include "../SPA/PQL/Utilities/PatternClause.h"
+#include "../SPA/PQL/Utilities/WithClause.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{
+
+    TEST_CLASS(TestClauseGroupManager)
+    {
+    public:
+
+        TEST_METHOD(TestSetAndGetClauseGroupQueue) {
+            /*
+            Query:
+            assign a1, a2; stmt s1, s2;
+            while w1, w2; if i1, if2; while w1, w2; call cl1, cl2;
+            procedure p1, p2; prog_line pl1, pl2;
+            variable v1, v2; constant c1, c2;
+
+            Select a1
+            such that Uses(a1, v1)
+            such that Modifies(w1, v1)
+            such that Uses(a2, v2)
+            such that Uses(10, "dummyVar")
+            */
+            SelectClause select_a1 = SelectClause(SelectionType::SELECT_SINGLE, Entity::ASSIGN, "a1");
+            SuchThatClause uses_a1_v1 = SuchThatClause(Relationship::USES,
+                Entity::ASSIGN, "a1",
+                Entity::VARIABLE, "v1");
+            SuchThatClause modifies_w1_v1 = SuchThatClause(Relationship::MODIFIES,
+                Entity::WHILE, "w1",
+                Entity::VARIABLE, "v1");
+            SuchThatClause uses_a2_v2 = SuchThatClause(Relationship::USES,
+                Entity::ASSIGN, "a2",
+                Entity::VARIABLE, "v2");
+            SuchThatClause uses_10_ident = SuchThatClause(Relationship::USES,
+                Entity::INTEGER, "10",
+                Entity::IDENT_WITHQUOTES, "dummyVar");
+
+            queue<queue<Clause>> clauseGroupQueue;
+            queue<Clause> tempClauseQueue;
+
+            // TODO: Continue implemneting
+        }
+    };
+}
