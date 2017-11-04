e448978a3ab16ee6b03a872fac5168f22bf03679
“marcusngwj” <marcusngwj@msn.com>
2017-11-04 22:06:15 +0800

    Update test 13

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/Release/AutoTester.bat
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/Release/AutoTester.bat
@@ -560,10 +560,10 @@ echo Finish running AutoTester for Query3.txt.
 ::AutoTester Test13_Seven\4_Intense\Source4.txt Test13_Seven\4_Intense\Query4.txt TestResult\out13_Intense_Query4.xml > TestResult\cmd\cmd13_Intense_Query4.txt
 ::echo Finish running AutoTester for Query4.txt.
 echo -- TIMEOUT! -- Query4.txt has been commented away in the bat file -- ! --
-REM echo Running AutoTester for Query5.txt...
-REM AutoTester Test13_Seven\4_Intense\Source5.txt Test13_Seven\4_Intense\Query5.txt TestResult\out13_Intense_Query5.xml > TestResult\cmd\cmd13_Intense_Query5.txt
-REM echo Finish running AutoTester for Query5.txt.
-echo -- TIMEOUT! -- Query5.txt has been commented away in the bat file -- ! --
+echo Running AutoTester for Query5.txt...
+AutoTester Test13_Seven\4_Intense\Source5.txt Test13_Seven\4_Intense\Query5.txt TestResult\out13_Intense_Query5.xml > TestResult\cmd\cmd13_Intense_Query5.txt
+echo Finish running AutoTester for Query5.txt.
+REM echo -- TIMEOUT! -- Query5.txt has been commented away in the bat file -- ! --
 echo Running AutoTester for Query6a.txt...
 AutoTester Test13_Seven\4_Intense\Source6.txt Test13_Seven\4_Intense\Query6a.txt TestResult\out13_Intense_Query6a.xml > TestResult\cmd\cmd13_Intense_Query6a.txt
 echo Finish running AutoTester for Query6a.txt.
