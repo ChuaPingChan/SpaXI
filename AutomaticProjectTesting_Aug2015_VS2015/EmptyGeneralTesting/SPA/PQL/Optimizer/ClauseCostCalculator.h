df8074afa23a73ddc32e23cc097b0d73320f4b5c
ChuaPingChan <chua_pingchan@u.nus.edu>
2017-11-04 14:37:32 +0800

    Add ClauseType enum in ClauseCostCalculator class.

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Optimizer/ClauseCostCalculator.h
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PQL/Optimizer/ClauseCostCalculator.h
@@ -16,7 +16,45 @@ private:
 
     enum ClauseType
     {
-        // TODO: Implement
+        PATTERN_BOOLEAN = 0,
+        FOLLOWS_BOOLEAN,
+        NEXT_BOOLEAN,
+        CALLS_BOOLEAN,
+        PARENT_BOOLEAN,
+        MODIFIES_BOOLEAN,
+        USES_BOOLEAN,
+        CALLS_STAR_BOOLEAN,
+        PARENT_STAR_BOOLEAN,
+        FOLLOW_STAR_BOOLEAN,
+
+        WITH_ANY_ARGS,
+        PATTERN_ANY_ARGS,
+        FOLLOWS_1ARG,
+        NEXT_ARGS,
+        CALLS_ARGS,
+        PARENT_ARGS,
+        MODIFIES_ARGS,
+        USES_ARGS,
+        CALLS_STAR_ARGS,
+        PARENT_STAR_ARGS,
+        FOLLOWS_STAR_ARGS,
+
+        CALLS_2ARGS,
+        CALLS_STAR_2ARGS,
+        PARENT_2ARGS,
+        MODIFIES_2ARGS,
+        USES_2ARGS,
+        FOLLOWS_2ARGS,
+        PARENT_STAR_2ARGS,
+        NEXT_2ARGS,
+        FOLLOWS_STAR_2ARGS,
+
+        NEXT_STAR_1ARG,
+        NEXT_STAR_2ARGS,
+        AFFECTS_1ARG,
+        AFFECTS_STAR_1ARG,
+        AFFECTS_2ARGS,
+        AFFECTS_STAR_2ARGS
     };
 
     unordered_map<ClauseCostCalculator::ClauseType, int> _costTable;
