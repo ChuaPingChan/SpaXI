11658af51abb312878e9eb0924935789367b2280
Ying <ying1996@gmail.com>
2017-11-07 23:01:49 +0800

    comments for cache and unit test

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PKB/Cache.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/SPA/PKB/Cache.cpp
@@ -5,11 +5,17 @@ using namespace std;
 Cache::Cache() {
 }
 
+/*
+This puts all the next star relation into the cache with specified entity
+*/
 bool Cache::putAllNextStar(pair<list<int>, list<int>> allNextStar, Entity type1, Entity type2) {
 	allNextStarPairMap[type1][type2] = allNextStar;
 	return true;
 }
 
+/*
+This sets the maps and reverse maps as well as the general relation map
+*/
 bool Cache::putAllNextStar(unordered_map<int, list<int>> nextStarMap, unordered_map<int, list<int>> nextStarMapReverse,
 	unordered_map<int, unordered_set<int>> nextStarRelMap) {
 	this->nextStarMap = nextStarMap;
@@ -30,18 +36,29 @@ unordered_map<int, unordered_set<int>> Cache::getNextStarRelMap() {
 	return nextStarRelMap;
 }
 
+/*
+Checks if the next star relations pairs are already stored in cache
+*/
 bool Cache::containsAllNextStar(Entity type1, Entity type2) {
 	return (allNextStarPairMap.find(type1) != allNextStarPairMap.end() &&
 		allNextStarPairMap[type1].find(type2) != allNextStarPairMap[type1].end());
 }
 
+/*
+Get all next star relations pair
+*/
 pair<list<int>, list<int>> Cache::getAllNextStar(Entity type1, Entity type2) {
-	if (!containsAllNextStar(type1, type2)) {
+	if (!containsAllNextStar(type1, type2)) { //safety check
 		return make_pair(list<int>(), list<int>());
 	}
 	return allNextStarPairMap[type1][type2];
 }
 
+/*
+Adds the affects relation pair and relation map into cache
+Does not need to take into account entity as affects is only for
+assignment statements
+*/
 bool Cache::putAllAffects(pair<list<int>, list<int>> allAffects, unordered_map<int, unordered_set<int>> affectsRelMap) {
 	this->allAffectsPair = allAffects;
 	this->affectsRelMap = affectsRelMap;
@@ -57,6 +74,9 @@ pair<list<int>, list<int>> Cache::getAllAffects() {
 	return allAffectsPair;
 }
 
+/*
+Puts all relevant information regarding affects* into the cache
+*/
 bool Cache::putAllAffectsStar(pair<list<int>, list<int>> allAffectsStar,
 	unordered_map<int, unordered_set<int>> affectsStarRelMap, unordered_map<int, list<int>> affectsStarMap,
 	unordered_map<int, list<int>> affectsStarMapReverse) {
@@ -92,6 +112,9 @@ unordered_map<int, unordered_set<int>> Cache::getAllAffectsRelMap() {
 	return affectsRelMap;
 }
 
+/*
+Puts the affects relation into cache with the case of same synonym
+*/
 bool Cache::putAllAffectsSameSyn(list<int> allAffectsSameSynList) {
 	allAffectsSameSyn = allAffectsSameSynList;
 	hasAllAffectsSameSyn = true;
@@ -106,6 +129,9 @@ list<int> Cache::getAllAffectsSameSyn() {
 	return allAffectsSameSyn;
 }
 
+/*
+Puts the affects* relation into cache with the case of same synonym
+*/
 bool Cache::putAllAffectsStarSameSyn(list<int> allAffectsStarSameSynList) {
 	allAffectsStarSameSyn = allAffectsStarSameSynList;
 	hasAllAffectsStarSameSyn = true;
