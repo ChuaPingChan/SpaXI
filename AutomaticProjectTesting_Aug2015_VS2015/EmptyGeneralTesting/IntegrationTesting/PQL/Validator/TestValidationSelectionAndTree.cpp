6d37b256cd8afbf20e847e1d85298a1a60008877
“marcusngwj” <marcusngwj@msn.com>
2017-11-11 02:54:58 +0800

    selection

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/PQL/Validator/TestValidationSelectionAndTree.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/PQL/Validator/TestValidationSelectionAndTree.cpp
@@ -12,24 +12,37 @@ namespace UnitTesting
     TEST_CLASS(TestValidationSelectionAndTree)
     {
     public:
+        string rawStr;
+        string extracted;
+        string actual;
+        QueryTree *qtPtr;
+        FriendSelectionValidator sdv = FriendSelectionValidator(qtPtr);
+
+        TEST_METHOD_INITIALIZE(Test_Initialisation)
+        {
+            qtPtr = new QueryTree;
+            sdv = FriendSelectionValidator(qtPtr);
+        }
+
+        TEST_METHOD_CLEANUP(Test_Cleanup)
+        {
+            delete qtPtr;
+            qtPtr = NULL;
+        }
 
         TEST_METHOD(TestExtraction_Selection_SingleSynonym_SuchThatClause)
         {
-            QueryTree qtPtr;
-            FriendSelectionValidator sdv = FriendSelectionValidator(&qtPtr);
-            string rawStr = "Select c such that Follows(1,2)";
-            string extracted = sdv.extractRawString(rawStr);
-            string actual = "Select c ";
+            rawStr = "Select c such that Follows(1,2)";
+            extracted = sdv.extractRawString(rawStr);
+            actual = "Select c ";
             Assert::AreEqual(extracted, actual);
         }
 
         TEST_METHOD(TestExtraction_Selection_SingleSynonymAttribute_SuchThatClause)
         {
-            QueryTree qtPtr;
-            FriendSelectionValidator sdv = FriendSelectionValidator(&qtPtr);
-            string rawStr = "Select c.procName such that Follows(1,2)";
-            string extracted = sdv.extractRawString(rawStr);
-            string actual = "Select c.procName ";
+            rawStr = "Select c.procName such that Follows(1,2)";
+            extracted = sdv.extractRawString(rawStr);
+            actual = "Select c.procName ";
             Assert::AreEqual(extracted, actual);
         }
 
