66b5ee433ec4f6c005ae0dfc97b4090eabc28a36
“marcusngwj” <marcusngwj@msn.com>
2017-11-11 03:02:04 +0800

    update tests

--- AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/PQL/ResultFormatter/TestResultFormatterWithClauseResultAndQueryTree.cpp
+++ AutomaticProjectTesting_Aug2015_VS2015/EmptyGeneralTesting/IntegrationTesting/PQL/ResultFormatter/TestResultFormatterWithClauseResultAndQueryTree.cpp
@@ -14,69 +14,73 @@ namespace UnitTesting
 	TEST_CLASS(TestResultFormatterWithClauseResultAndQueryTree)
 	{
 	public:
+        QueryTree *qtPtr;
+        ClauseResult cr;
+        ResultFormatter rf;
+
+        TEST_METHOD_INITIALIZE(Test_Initialisation)
+        {
+            qtPtr = new QueryTree;
+            cr = ClauseResult();
+            rf = ResultFormatter();
+        }
+
+        TEST_METHOD_CLEANUP(Test_Cleanup)
+        {
+            delete qtPtr;
+            qtPtr = NULL;
+        }
 
 		TEST_METHOD(TestResultFormatter_SelectBOOLEAN_ClauseResultNonEmpty_True)
 		{
-			QueryTree qt;
 			SelectClause expected = UtilitySelection::makeSelectClause(SELECT_BOOLEAN);
-			qt.insertSelect(UtilitySelection::makeSelectClause(SELECT_BOOLEAN));
-			Assert::IsTrue(UtilitySelection::isSameSelectClauseContent(expected, qt.getSelectClause()));
+			qtPtr->insertSelect(UtilitySelection::makeSelectClause(SELECT_BOOLEAN));
+			Assert::IsTrue(UtilitySelection::isSameSelectClauseContent(expected, qtPtr->getSelectClause()));
 			string synonym = "s";
 			list<int> synonymResult = { 1,2,3 };
-			ClauseResult cr;
 			cr.updateSynResults(synonym, synonymResult);
-			ResultFormatter rf;
-			Assert::IsTrue(rf.finalResultFromSelection(cr, qt).size()==1);
-			Assert::IsTrue(rf.finalResultFromSelection(cr, qt).front()=="true");
+			Assert::IsTrue(rf.finalResultFromSelection(cr, *qtPtr).size()==1);
+			Assert::IsTrue(rf.finalResultFromSelection(cr, *qtPtr).front()=="true");
 		}
         
 		TEST_METHOD(TestResultFormatter_SelectBOOLEAN_ClauseResultEmpty_False)
 		{
-			QueryTree qt;
 			SelectClause expected = UtilitySelection::makeSelectClause(SELECT_BOOLEAN);
-			qt.insertSelect(UtilitySelection::makeSelectClause(SELECT_BOOLEAN));
-			Assert::IsTrue(UtilitySelection::isSameSelectClauseContent(expected, qt.getSelectClause()));
-			ClauseResult cr;
-			ResultFormatter rf;
-			Assert::IsTrue(rf.handleNoResult(qt).size() == 1);
-			Assert::IsTrue(rf.handleNoResult(qt).front() == "false");
+			qtPtr->insertSelect(UtilitySelection::makeSelectClause(SELECT_BOOLEAN));
+			Assert::IsTrue(UtilitySelection::isSameSelectClauseContent(expected, qtPtr->getSelectClause()));
+			Assert::IsTrue(rf.handleNoResult(*qtPtr).size() == 1);
+			Assert::IsTrue(rf.handleNoResult(*qtPtr).front() == "false");
 		}
 
 		TEST_METHOD(TestResultFormatter_SelectSynonym_ClauseResultNonEmpty_OneResult_ResultExpected)
 		{
-			QueryTree qt;
 			string synonym = "s";
 			SelectClause expected = UtilitySelection::makeSelectClause(SELECT_SINGLE, STMT, "s");
-			qt.insertSelect(UtilitySelection::makeSelectClause(SELECT_SINGLE, STMT, synonym));
-			Assert::IsTrue(UtilitySelection::isSameSelectClauseContent(expected, qt.getSelectClause()));
+			qtPtr->insertSelect(UtilitySelection::makeSelectClause(SELECT_SINGLE, STMT, synonym));
+			Assert::IsTrue(UtilitySelection::isSameSelectClauseContent(expected, qtPtr->getSelectClause()));
 			list<int> synonymResult = { 1,2,3 };
-			ClauseResult cr;
 			cr.updateSynResults(synonym, synonymResult);
 			Assert::IsTrue(cr.hasResults());			
 			list<string> expectedResult = { "1","2","3" };
-			ResultFormatter rf;
-			list<string> actualResult = rf.finalResultFromSelection(cr, qt);
+			list<string> actualResult = rf.finalResultFromSelection(cr, *qtPtr);
 			Assert::IsTrue(actualResult.size() == 3);
 		    Assert::IsTrue(actualResult == expectedResult);
 		}
 
 		TEST_METHOD(TestResultFormatter_SelectSynonym_ClauseResultNonEmpty_TwoResults_ResultExpected)
 		{
-			QueryTree qt;
 			string synonym1 = "s1";
 			string synonym2 = "s2";
 			SelectClause expected = UtilitySelection::makeSelectClause(SELECT_SINGLE, STMT, "s1");
-			qt.insertSelect(UtilitySelection::makeSelectClause(SELECT_SINGLE, STMT, synonym1));
-			Assert::IsTrue(UtilitySelection::isSameSelectClauseContent(expected, qt.getSelectClause()));
+			qtPtr->insertSelect(UtilitySelection::makeSelectClause(SELECT_SINGLE, STMT, synonym1));
+			Assert::IsTrue(UtilitySelection::isSameSelectClauseContent(expected, qtPtr->getSelectClause()));
 			list<int> synonym1Result = { 1,2 };
 			list<int> synonym2Result = { 2,3, 4 };
-			ClauseResult cr;
 			cr.updateSynResults(synonym1, synonym1Result);
 			cr.updateSynResults(synonym2, synonym2Result);
 			Assert::IsTrue(cr.hasResults());
 			list<string> expectedResult = { "1","2" };
-			ResultFormatter rf;
-			list<string> actualResult = rf.finalResultFromSelection(cr, qt);
+			list<string> actualResult = rf.finalResultFromSelection(cr, *qtPtr);
 			Assert::IsTrue(actualResult.size() == 2);
 			Assert::IsTrue(actualResult == expectedResult);
 		}
